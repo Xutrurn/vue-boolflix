eb98a891df7ec5bec3b92cb936b03a2d71a75979
Xutrurn <kanjoskillz@gmail.com>
2021-02-11 14:53:47 +0100

    aggiunti voti e bandiere

--- js/script.js
+++ js/script.js
@@ -1,24 +1,56 @@
-var app = new Vue({
-  el: '#app',
+let app = new Vue({
+  el: "#app",
   data: {
-    searchMovies: '',
     apiKey: '7a3c07bb92f9c591e83f48a742e9a7e2',
-    allMovies: [],
+    ricerca: "",
+    films: [],
+    tvSeries: [],
+
+
   },
   methods: {
-    getMovies() {
-      axios.get('https://api.themoviedb.org/3/search/movie', {
+    datiScaricati() {
+      axios
+        .get("https://api.themoviedb.org/3/search/movie", {
           params: {
             api_key: this.apiKey,
-            query: this.searchMovies,
+            query: this.ricerca,
+            language: "it-IT",
+            include_adult: false
           }
         })
-        .then((result) => {
-          this.allMovies = result.data.results;
+        .then(result => {
+          this.films = result.data.results
+
+          this.films.forEach(item => {
+            const vote = Math.ceil(item.vote_average / 2)
+            console.log(vote);
+            return item.stellePiene = vote;
+          });
         })
-    },
-  },
-  mounted() {
-    console.log('hello world');
+
+
+
+      axios
+        .get("https://api.themoviedb.org/3/search/tv", {
+          params: {
+            api_key: this.apiKey,
+            query: this.ricerca,
+            language: "it-IT",
+            include_adult: false
+          }
+        })
+        .then(result => {
+          this.tvSeries = result.data.results
+
+          this.tvSeries.forEach(item => {
+            const vote = Math.ceil(item.vote_average / 2)
+            console.log(vote);
+            return item.stellePiene = vote;
+          });
+        })
+
+
+    }
   }
-});
+})
